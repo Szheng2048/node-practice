475963fc3700dc09f92eed22efb363f000d28bbd
Steven Zheng <szheng.persevere@gmail.com>
2023-12-04 11:42:46 -0600

    yo

--- nodePractice.js
+++ nodePractice.js
@@ -1,7 +1,7 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
 
-console.log("Hello, " + getInput(1) + " " + getInput(2))
-console.log("I'm capitalizing " + getInput(1).toUpperCase() + " " + getInput(2).toUpperCase() + "! Is it capital?")
-console.log("These are your initials " + getInput(1)[0] + getInput(2)[0])
-console.log("This is my Email: " + getInput(1)[0].toLowerCase() + getInput(2).toLowerCase() + ".prsvr@gmail.com.")
+console.log("Hello " + getInput(1)[0].toUpperCase() + getInput(1).slice(1).toLowerCase() + " " + getInput(2)[0].toUpperCase() + getInput(2).slice(1).toLowerCase())
+console.log(getInput(1).toUpperCase() + " " + getInput(2).toUpperCase() + " is your name capitalized.")
+console.log(getInput(1)[0].toUpperCase() + ". " + getInput(2)[0].toUpperCase() + ".")
+console.log(getInput(1)[0].toLowerCase() + getInput(2).toLowerCase() + ".prsvr@gmail.com.")
